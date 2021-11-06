f126ef3665e3adf3bd7923cf1e574c8875ac4d97
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-11-06 14:19:50 +0100

    ok

--- system/kernel.js
+++ system/kernel.js
@@ -12,18 +12,18 @@
 
 let oos = {
   main: () => {
-    window.addEventListener("load", async () => {
+    window.addEventListener("load", () => {
       let osDiv = document.querySelector(".os-container");
 
       /*/ import CSS /*/
-      await fetch("https://open-os.netlify.app");
-
-      oos.util.import.css(
-        "https://open-os.netlify.app/system/stylesheets/normaliser.css"
-      );
-      oos.util.import.css(
-        "https://open-os.netlify.app/system/stylesheets/master.css"
-      );
+      fetch("https://open-os.netlify.app").then(() => {
+        oos.util.import.css(
+          "https://open-os.netlify.app/system/stylesheets/normaliser.css"
+        );
+        oos.util.import.css(
+          "https://open-os.netlify.app/system/stylesheets/master.css"
+        );
+      });
 
       osDiv.innerHTML = `
             <div class='loader'>
