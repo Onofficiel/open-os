365606f29832891a1f0c63a6ca2bdeadd54593b6
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-11-07 14:56:36 +0100

    ok

--- system/kernel.js
+++ system/kernel.js
@@ -679,6 +679,35 @@ let oos = {
         .removeChild(this.notifDiv);
     }
   },
-  FS: class {},
+  FS: class {
+    constructor() {
+      let db = "";
+      let openReq = indexedDB.open("fs", 1);
+
+      openReq.onupgradeneeded = () => {
+        db = openReq.result;
+
+        const store = db.createObjectStore("users", { keyPath: "path" });
+
+        console.log(store);
+        store.add({
+          path: "/test/hello.js",
+          content: "Hello, World!",
+        });
+      };
+
+      openReq.onerror = () => {
+        console.error("Error while accessing FileSystem");
+      };
+
+      openReq.onblocked = function (event) {
+        alert("Please close all other tabs with Open OS open!");
+      };
+
+      openReq.onsuccess = () => {
+        db = openReq.result;
+      };
+    }
+  },
 };
 oos.main();
