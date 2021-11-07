d73d7d57b5ee6e1c82f2815beb8576a18f2c6e08
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-11-07 17:48:52 +0100

    ok

--- system/kernel.js
+++ system/kernel.js
@@ -724,6 +724,9 @@ let oos = {
         req.onsuccess = function () {
           resolve(req.result.content);
         };
+        req.onerror = function (e) {
+          reject(e);
+        }
       });
     }
   },
