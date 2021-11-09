52a9afa24cacadec16f8b0a3905b8b3f24b9efef
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-11-09 19:16:51 +0100

    ok

--- system/kernel.js
+++ system/kernel.js
@@ -778,14 +778,16 @@ let oos = {
     }
 
     correctPath(path) {
-      if (!path.startsWith("/")) path = this.currentDirrectory + path;
-      if (path.endsWith("/") && path !== "/") path = path.slice(0, path.length - 1);
+      if (!path.startsWith("/")) path = this.currentDirrectory + "/" + path;
+      if (path.endsWith("/") && path !== "/")
+        path = path.slice(0, path.length - 1);
 
       return path;
     }
 
     changedir(path) {
-      if (path.endsWith("/") && path !== "/") path = path.slice(0, path.length - 1);
+      if (path.endsWith("/") && path !== "/")
+        path = path.slice(0, path.length - 1);
 
       if (path.startsWith("/")) {
         this.isFile(path).then((r) => {
