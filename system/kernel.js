d9c4ac9ccac351a69435b9d9cd9b289ea9c2c433
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-11-09 19:35:43 +0100

    ok

--- system/kernel.js
+++ system/kernel.js
@@ -778,7 +778,8 @@ let oos = {
     }
 
     correctPath(path) {
-      if (!path.startsWith("/") && path !== "/") path = this.currentDirectory + "/" + path;
+      if (!path.startsWith("/") && this.currentDirectory !== "/")
+        path = this.currentDirectory + "/" + path;
       if (path.endsWith("/") && path !== "/")
         path = path.slice(0, path.length - 1);
 
