27f31e21a5ab01e1462439b75ce07b40db7db5ce
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-11-09 18:30:56 +0100

    ok

--- system/kernel.js
+++ system/kernel.js
@@ -780,6 +780,7 @@ let oos = {
 
       if (path.startsWith("/")) {
         this.isFile(path).then((r) => {
+          console.log(r);
           if (r) throw new Error("Can't change directory, not a directory");
           return (this.currentDirrectory = path);
         });
