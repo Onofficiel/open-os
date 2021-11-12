22ecd3f083e50ac807286059a54afd3d05f2d8d0
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-11-12 13:06:12 +0100

    ok

--- system/kernel.js
+++ system/kernel.js
@@ -1085,7 +1085,7 @@ let oos = {
                 if (el.startsWith("/")) el = el.substring(1);
                 el = el.split("/")[0];
 
-                if (el.indexOf(paths) <= -1) paths.push(el);
+                if (path.indexOf(el) == -1) paths.push(el);
               }
             }
           }
