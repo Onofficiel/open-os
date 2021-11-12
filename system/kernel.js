07ab42188e84b676663ed053db13dd41873087ba
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-11-12 12:33:46 +0100

    ok

--- system/kernel.js
+++ system/kernel.js
@@ -1080,8 +1080,8 @@ let oos = {
               let el = Object.keys(req.result[0].data)[cPath];
 
               if (el.startsWith(path)) {
-                if (el.startsWith("/")) el.split("/").splice(0, 1);
-                
+                if (el.startsWith("/")) el = el.split("/").splice(0, 1).join("/");
+
                 console.log(el.split("/")[path.split("/").length - 1]);
               }
             }
