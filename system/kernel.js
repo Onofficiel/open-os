7b682fdebc797f1a99573cd327e717ed50bba1df
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-11-12 14:55:04 +0100

    ok

--- system/kernel.js
+++ system/kernel.js
@@ -1075,11 +1075,9 @@ let oos = {
 
           for (const cPath in req.result[0].data) {
             if (Object.hasOwnProperty.call(req.result[0].data, cPath)) {
-              let elType = req.result[0].data[cPath];
+              let elType = req.result[0].data[cPath].type;
               let el = Object.keys(req.result[0].data)[cPath];
 
-              console.log(elType);
-              
               if (el.startsWith(path)) {
                 el = el.slice(path.length);
 
