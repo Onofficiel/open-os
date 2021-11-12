0cce57a1e811022761097c57d81ddcf38d6f3c55
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-11-12 14:43:34 +0100

    ok

--- system/kernel.js
+++ system/kernel.js
@@ -1077,6 +1077,7 @@ let oos = {
             if (
               Object.hasOwnProperty.call(Object.keys(req.result[0].data), cPath)
             ) {
+              console.log(cPath);
               let elType = req.result[0].data[cPath];
               let el = Object.keys(req.result[0].data)[cPath];
 
