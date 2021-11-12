2e80ccbdcc24ebfab229666006a86bb36a85ea4c
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-11-12 15:34:55 +0100

    ok

--- system/kernel.js
+++ system/kernel.js
@@ -1073,7 +1073,7 @@ let oos = {
         req.onsuccess = () => {
           let paths = [];
 
-          console.log("Ok");
+          console.log(req);
           for (let i = 0; i < req.result[0].data.length; i++) {
             let el = Object.keys(req.result[0].data)[i];
 
