472b212f6595dc7a96e5fe6a1548dbf42c67b4d6
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-11-17 22:27:14 +0100

    ok

--- system/stylesheets/normaliser.css
+++ system/stylesheets/normaliser.css
@@ -2,7 +2,7 @@
  *                                *
  *             OpenOS             *
  *                                *
- *    Copyright (c) Onofficiel    *
+ *           Onofficiel           *
  *                                *
 /*********************************/
 
