c9866c72d581d2ed6ac53e9b186ac2d40a94268f
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-11-19 10:22:47 +0100

    ok

--- system/kernel.js
+++ system/kernel.js
@@ -1087,7 +1087,7 @@ let oos = {
               if (
                 el.startsWith(path) &&
                 el.split("/").length == path.split("/").length &&
-                paths.indexOf(path) != -1
+                paths.indexOf(path) == -1
               ) {
                 paths.push(path);
               }
