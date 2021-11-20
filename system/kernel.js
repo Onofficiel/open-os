9c2ce8a4ee4c70e9e1ce6b4448101cb71352dc20
Kelbaz <81097895+Kodbaz@users.noreply.github.com>
2021-11-20 20:15:04 +0100

    ok

--- system/kernel.js
+++ system/kernel.js
@@ -317,7 +317,7 @@ let oos = {
           if (cmd === cCmd.name) return cCmd.exec();
         }
       }
-      throw "Command not found.";
+      throw new Error("Command not found.");
     },
   },
   /**
